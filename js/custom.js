e93963d8e25104a5668a9aa2ff73fcf0be68e38b
Stefan Fritzon <stefan.fritzon@gmail.com>
2019-07-23 22:05:48 +0200

    update pics and map

--- js/custom.js
+++ js/custom.js
@@ -269,11 +269,11 @@
 
 	var dateCountDown = function() {
 		$('.date-countdown').simplyCountdown({
-	    year: 2019, // year
-	    month: 4, // month
-	    day: 20, // day
-	    hours: 0, // Default is 0 [0-23] integer
-      minutes: 0, // Default is 0 [0-59] integer
+	    year: 2020, // year
+	    month: 7, // month
+	    day: 4, // day
+	    hours: 13, // Default is 0 [0-23] integer
+      minutes: 30, // Default is 0 [0-59] integer
       seconds: 0, // Default is 0 [0-59] integer
 		});
 	};
